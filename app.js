8c0f3105f8b48e2cdf9858aed4f8c743acc63d5a
gylaandrij <gyla.andrij@gmail.com>
2017-06-29 18:35:39 +0300

    todo db

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
 var todoController = require('./controllers/todoControllers');
+var helmet = require('helmet');
 
 app.set('view engine', 'ejs');
+
 app.use(express.static('./public/assets'));
+app.use(helmet());
 
 todoController(app);
 
